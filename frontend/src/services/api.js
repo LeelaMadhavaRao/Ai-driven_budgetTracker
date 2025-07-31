afc96123c93695e2a0b4afd284f306e2e85d81af
LeelaMadhavaRao <leelamadhav.nulakani@gmail.com>
2025-07-31 11:53:24 +0530

    ....

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_BASE_URL = "https://ai-driven-budget-tracker.vercel.app/api/";
+const API_BASE_URL = "https://ai-driven-budget-trackerbackend.vercel.app/api/";
 
 // Create axios instance
 const api = axios.create({
